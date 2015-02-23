f720c21659c626b56bae982807cf853b73b83a4e
Will Drach <widr1225@colorado.edu>
2015-02-23 15:17:00 -0700

    Added modified blink for lab 4

---
+++ Blink_modified/Blink_modified.ino
@@ -0,0 +1,19 @@
+#define BUT 1
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+  pinMode(BUT, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  boolean buttonOn = digitalRead(BUT);
+  if (buttonOn){
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);              // wait for 2 secs
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);              // wait for .2 secs
+  }
+}
