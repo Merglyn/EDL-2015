a2c8e83733fc53d5752d107219276f28ec6bd3bc
Will Drach <widr1225@colorado.edu>
2015-04-06 14:17:59 -0600

    had some leftovers in pulseRead

--- pulseRead/pulseRead.ino
+++ pulseRead/pulseRead.ino
@@ -1,26 +1,12 @@
 const int pulsePin = 5;
 
-// define pins
-const int pinON = 6;
-const int pinCC_Right = 8;
-const int pinCW_Right = 9;
-const int pinSpeed_Right = 7;
-const int pinCW_Left = 10;
-const int pinCC_Left = 11;
-const int pinSpeed_Left = 12;
-
-// define vars
-volatile unsigned int enc_left = 0;
-volatile unsigned int enc_right = 0;
-
-
 //define some useful vars
-unsigned long tmp_pulse = 0;
 unsigned long cur_pulse = 0;
+
 void setup() {
   //set pinmodes
   pinMode(13, OUTPUT);
-  pinMode(PIN, INPUT);
+  pinMode(pulsePin, INPUT);
 
   //begin serial
   Serial.begin(9600);
@@ -47,7 +33,7 @@ void loop() {
 void readPulse(){
     cur_pulse = 0;
     while(cur_pulse < 50000){
-        cur_pulse = pulseIn(PIN, HIGH);
+        cur_pulse = pulseIn(pulsePin, HIGH);
     }
     return;
 }
